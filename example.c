dfdf30dcb1f116e3789403b0471edcc3a9521e5e
0110 <github@buxs.org>
2012-01-13 20:44:26 +0100

    Send LEDs ON

--- example.c
+++ example.c
@@ -6,14 +6,19 @@
 
 int main(int argc, char *argv[]) {
 
-	if (argc != 2) { printf("Usage: server\n");
+	int i;
+	
+	if (argc != 2) { 
+		printf("Usage: server\n");
 		return 1;
 	}
 	
 	artnet_send_t* universe;
 	
 	universe = artnet_send_create(argv[1], 0, 512);
-
+	for (i=0; i < 4; i++) {
+		universe->data[i] = 255;
+	}
 	artnet_send_send(universe);
 	
     return 0;
