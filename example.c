d253b7c80d317d362c27ee3a3aa60c9569c53c7c
0110 <github@buxs.org>
2012-01-13 20:36:23 +0100

    send artnet

--- example.c
+++ example.c
@@ -6,7 +6,15 @@
 
 int main(int argc, char *argv[]) {
 
+	if (argc != 2) { printf("Usage: server\n");
+		return 1;
+	}
 	
+	artnet_send_t* universe;
+	
+	universe = artnet_send_create(argv[1], 0, 512);
+
+	artnet_send_send(universe);
 	
     return 0;
 }
